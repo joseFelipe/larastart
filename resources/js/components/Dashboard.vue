6e754c12fcc4d28c9f97b943a234b4c4e2f00ebd
Felipe <felipe.florianopolis@gmail.com>
2019-04-22 21:51:07 -0300

    Implemented all navigation with VueRoutes. Implemented the login and logout too.

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,301 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Transações do Período</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+            <div class="row">
+              <div class="col-sm-12 col-md-12">
+                <div class="dataTables_length" id="example1_length">
+                  <label>
+                    Mostrar
+                    <select
+                      name="example1_length"
+                      aria-controls="example1"
+                      class="custom-select custom-select-sm form-control form-control-sm"
+                    >
+                      <option value="10">10</option>
+                      <option value="25">25</option>
+                      <option value="50">50</option>
+                      <option value="100">100</option>
+                    </select> linhas
+                  </label>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-6">
+                <div id="example1_filter" class="dataTables_filter">
+                  <label>
+                    Pesquisar:
+                    <input
+                      type="search"
+                      class="form-control form-control-sm"
+                      placeholder
+                      aria-controls="example1"
+                    >
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12">
+                <table
+                  id="example1"
+                  class="table table-bordered table-striped dataTable"
+                  role="grid"
+                  aria-describedby="example1_info"
+                >
+                  <thead>
+                    <tr role="row">
+                      <th
+                        class="sorting_asc"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-sort="ascending"
+                        aria-label="Rendering engine: activate to sort column descending"
+                      >Transação</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="Browser: activate to sort column ascending"
+                      >Nome</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="Platform(s): activate to sort column ascending"
+                      >Status</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="Engine version: activate to sort column ascending"
+                      >Data</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Dt. Repasse</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Parcelas</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Cartão</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Recibo</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Taxa Adm.</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Taxa Transf.</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Valor</th>
+                      <th
+                        class="sorting"
+                        tabindex="0"
+                        aria-controls="example1"
+                        rowspan="1"
+                        colspan="1"
+                        aria-label="CSS grade: activate to sort column ascending"
+                      >Vl. Líquido</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr role="row" class="odd">
+                      <td class="sorting_1">00001</td>
+                      <td>Maria dos Santos</td>
+                      <td>Disponível</td>
+                      <td>05/03/2019</td>
+                      <td>05/04/2019</td>
+                      <td>1/1</td>
+                      <td>**** **** **** 4321 8765</td>
+                      <td>...</td>
+                      <td>1,00</td>
+                      <td>0,20</td>
+                      <td>100,00</td>
+                      <td>100,00</td>
+                    </tr>
+                    <tr role="row" class="even">
+                      <td class="sorting_1">00002</td>
+                      <td>João da Silva</td>
+                      <td>Liquidado</td>
+                      <td>01/02/2019</td>
+                      <td>01/03/2019</td>
+                      <td>1/1</td>
+                      <td>**** **** **** 1234 5678</td>
+                      <td>...</td>
+                      <td>0,60</td>
+                      <td>0,20</td>
+                      <td>60,00</td>
+                      <td>60,00</td>
+                    </tr>
+                    <tr role="row" class="odd">
+                      <td class="sorting_1">00003</td>
+                      <td>Fernando Duwe</td>
+                      <td>Indisponível</td>
+                      <td>22/01/2019</td>
+                      <td>22/02/2019</td>
+                      <td>1/3</td>
+                      <td>**** **** **** 1290 3496</td>
+                      <td>...</td>
+                      <td>1,30</td>
+                      <td>0,20</td>
+                      <td>130,00</td>
+                      <td>130,00</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12 col-md-5">
+                <div
+                  class="dataTables_info"
+                  id="example1_info"
+                  role="status"
+                  aria-live="polite"
+                >Mostrando 1 à 10 de 57 transações</div>
+              </div>
+              <div class="col-sm-12 col-md-7">
+                <div class="dataTables_paginate paging_simple_numbers" id="example1_paginate">
+                  <ul class="pagination">
+                    <li class="paginate_button page-item previous disabled" id="example1_previous">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="0"
+                        tabindex="0"
+                        class="page-link"
+                      >Anterior</a>
+                    </li>
+                    <li class="paginate_button page-item active">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="1"
+                        tabindex="0"
+                        class="page-link"
+                      >1</a>
+                    </li>
+                    <li class="paginate_button page-item">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="2"
+                        tabindex="0"
+                        class="page-link"
+                      >2</a>
+                    </li>
+                    <li class="paginate_button page-item">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="3"
+                        tabindex="0"
+                        class="page-link"
+                      >3</a>
+                    </li>
+                    <li class="paginate_button page-item">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="4"
+                        tabindex="0"
+                        class="page-link"
+                      >4</a>
+                    </li>
+                    <li class="paginate_button page-item">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="5"
+                        tabindex="0"
+                        class="page-link"
+                      >5</a>
+                    </li>
+                    <li class="paginate_button page-item">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="6"
+                        tabindex="0"
+                        class="page-link"
+                      >6</a>
+                    </li>
+                    <li class="paginate_button page-item next" id="example1_next">
+                      <a
+                        href="#"
+                        aria-controls="example1"
+                        data-dt-idx="7"
+                        tabindex="0"
+                        class="page-link"
+                      >Próximo</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-body -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,134 +11,138 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
   <title>Alternative Sistemas</title>
 
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-    <div class="wrapper">
+    <div class="wrapper" id="app">
 
-    <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
-            </li>
-        </ul>
+        <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+                </li>
+            </ul>
 
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
-            <div class="input-group input-group-sm">
-                <input class="form-control form-control-navbar" type="search" placeholder="Pesquisar" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-navbar" type="submit">
-                        <i class="fa fa-search"></i>
-                    </button>
+            <!-- SEARCH FORM -->
+            <form class="form-inline ml-3">
+                <div class="input-group input-group-sm">
+                    <input class="form-control form-control-navbar" type="search" placeholder="Pesquisar" aria-label="Search">
+                    <div class="input-group-append">
+                        <button class="btn btn-navbar" type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </nav>
+
+        <aside class="main-sidebar sidebar-light-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+                <img src="./img/logo.png" alt="Alternative Logo" class="brand-image img-circle elevation-3"
+                    style="opacity: .8">
+                <span class="brand-text font-weight-light">SIDESC</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+                            with font-awesome or any other icon font library -->
+
+                        <li class="nav-item">
+                            <router-link to="/dashboard" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Dashboard</p>
+                            </router-link>
+                        </li>
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="fas fa-cog"></i>
+                                <p>
+                                    Gerenciamento <i class="right fa fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link active">
+                                        <i class="fa fa-circle-o nav-icon"></i>
+                                        <p>Active Page</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="fa fa-circle-o nav-icon"></i>
+                                        <p>Inactive Page</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/profile" class="nav-link">
+                                <i class="nav-icon fa fa-user"></i>
+                                <p>Perfil</p>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                <i class="nav-icon fas fa-power-off"></i>
+                                <p> {{ __('Sair') }} </p>
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Main content -->
+            <div class="content">
+                <div class="container-fluid">
+                    <router-view></router-view>
                 </div>
             </div>
-        </form>
-    </nav>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
 
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-            <img src="./img/logo.png" alt="Alternative Logo" class="brand-image img-circle elevation-3"
-                style="opacity: .8">
-            <span class="brand-text font-weight-light">SIDESC</span>
-        </a>
-
-        <!-- Sidebar -->
-        <div class="sidebar">
-            <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-                </div>
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+                Cristo Vive!
             </div>
-
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <!-- Add icons to the links using the .nav-icon class
-                        with font-awesome or any other icon font library -->
-
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>Dashboard</p>
-                        </a>
-                    </li>
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link active">
-                            <i class="fas fa-cog"></i>
-                            <p>
-                                Management <i class="right fa fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link active">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>Active Page</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>Inactive Page</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-th"></i>
-                            <p>
-                                Simple Link <span class="right badge badge-danger">New</span>
-                            </p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
-                            <p>Profile</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-power-off"></i>
-                            <p>Logout</p>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-    </aside>
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Main content -->
-        <div class="content">
-            <div class="container-fluid">
-
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    <!-- Main Footer -->
-    <footer class="main-footer">
-        <!-- To the right -->
-        <div class="float-right d-none d-sm-inline">
-        Anything you want
-        </div>
-        <!-- Default to the left -->
-        <strong>Copyright &copy; 2014-2018 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-    </footer>
+            <!-- Default to the left -->
+            <strong>
+                Copyright &copy; 2019
+                <a href="https://alternativesistemas.com.br">Alternative Sistemas</a>.
+            </strong>
+        </footer>
     </div>
     <!-- ./wrapper -->
 
